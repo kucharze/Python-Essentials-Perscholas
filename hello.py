d7d078be460467d3b3a598751f024fd71d651a3c
Zachary-Kuchar <zacharyk323@live.com>
2026-09-21 19:19:26 -0400

    Added new print statements

--- hello.py
+++ hello.py
@@ -6,3 +6,7 @@ print(4+9)
 print(4.7/2)
 print("Hello World"*3)#Print out three hello world strings
 print('h','e','l','l','o',sep='$')
+
+#My print statements
+print("Welcome to the brand new world of Python")
+print("There once was someone who studyed Python \n and they loved it so much that they wanted to learn more about it \n and they did and they became a Python programmer")
