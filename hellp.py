23bacf014ae3543b18f460e024ec348aeb15f180
Zachary-Kuchar <zacharyk323@live.com>
2026-09-21 19:07:29 -0400

    Fixed misspelling

--- hellp.py
+++ hellp.py
@@ -4,4 +4,4 @@ print("I am studying Python Programming")
 
 print(4+9)
 print(4.7/2)
-print("Hellow World"*3)#Print out three hello world strings
+print("Hello World"*3)#Print out three hello world strings
